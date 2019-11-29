fcd982873b8d87393560871ae433ea6f873e9cc1
Nguyen_Hoang_Anh <hoanghuynh1998@gmail.com>
2019-11-29 20:11:27 +0700

    add modal to show detail work calendar in view

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -17,54 +17,62 @@
         @foreach($calendar as $week)
         <tr class="text-primary text-center">
             @foreach($week as $day)
-                <!-- chỉ có công việc đơn -->
-                @if($aday_works != null && $days_works == null)
+                @if($aday_works == null && $days_works == null) <!-- không có công việc trong tháng -->
                     @if($day == $today)
-                    <td id="day{{ $day }}" class="text-danger font-weight-bold">{{ $day }}</td>
-                    @else
-                    <td id="day{{ $day }}">{{ $day }}</td>
+                        <td id="day{{ $day }}" class="text-danger font-weight-bold">{{ $day }}</td>
+                        @else
+                        <td id="day{{ $day }}">{{ $day }}</td>
                     @endif
-                    @foreach($aday_works as $work)
-                        @if($day == (explode('-', $work->start_day)[2]))
-                            <i class="workActice" name="day{{ $day }}" title="{{ $work->description }}"></i>
-                        @endif
-                    @endforeach
-                <!-- chỉ có công việc kéo dài nhiều ngày -->
-                @elseif($aday_works == null && $days_works != null)
+                @else <!-- có công việc trong tháng -->
                     @if($day == $today)
-                    <td id="day{{ $day }}" class="text-danger font-weight-bold">{{ $day }}</td>
+                        <td id="day{{ $day }}" class="text-danger font-weight-bold">{{ $day }}</td>
                     @else
-                    <td id="day{{ $day }}">{{ $day }}</td>
+                        <td id="day{{ $day }}">{{ $day }}</td>
                     @endif
                     @foreach($days_works as $work)
                         @if($day >= (explode('-', $work->start_day)[2]) && $day <= (explode('-', $work->end_day)[2]))
-                            <i class="workActice" name="day{{ $day }}" title="{{ $work->description }}"></i>
-                        @endif
-                    @endforeach
-                <!-- // có cả công việc đơn và kéo dài -->
-                @elseif($aday_works != null && $days_works != null)
-                    @if($day == $today)
-                    <td id="day{{ $day }}" class="text-danger font-weight-bold">{{ $day }}</td>
-                    @else
-                    <td id="day{{ $day }}">{{ $day }}</td>
-                    @endif
-                     @foreach($days_works as $work)
-                        @if($day >= (explode('-', $work->start_day)[2]) && $day <= (explode('-', $work->end_day)[2]))
-                            <i class="workActice" name="day{{ $day }}" title="{{ $work->description }}"></i>
+                            <i class="workActice" name="day{{ $day }}" title="{{ $work->description }}" data-toggle="modal" data-target="#workDetail{{ $day }}"></i>
+                            <div class="modal fade" id="workDetail{{ $day }}" tabindex="-1" role="dialog" aria-labelledby="workDetail{{ $day }}Title" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title text-danger" id="workDetail{{ $day }}Title">
+                                                {{ $work->title }}
+                                            </h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            {{ $work->description }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         @endif
                     @endforeach
                     @foreach($aday_works as $work)
                         @if($day == (explode('-', $work->start_day)[2]))
-                            <i class="workActice" name="day{{ $day }}" title="{{ $work->description }}"></i>
+                            <i class="workActice" name="day{{ $day }}" title="{{ $work->description }}" data-toggle="modal" data-target="#workDetail{{ $day }}"></i>
+                            <div class="modal fade" id="workDetail{{ $day }}" tabindex="-1" role="dialog" aria-labelledby="workDetail{{ $day }}Title" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title text-danger" id="workDetail{{ $day }}Title">
+                                                {{ $work->title }}
+                                            </h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            {{ $work->description }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         @endif
                     @endforeach
-                <!-- không có công việc trong tháng -->
-                @elseif($aday_works == null && $days_works == null)
-                    @if($day == $today)
-                    <td id="day{{ $day }}" class="text-danger font-weight-bold">{{ $day }}</td>
-                    @else
-                    <td id="day{{ $day }}">{{ $day }}</td>
-                    @endif
                 @endif
             @endforeach
         </tr>
@@ -78,9 +86,13 @@
         for (var i = 0; i < day.length; i++) {
             var name = day[i].getAttribute("name");
             var title = day[i].getAttribute("title");
+            var data_toggle = day[i].getAttribute("data-toggle");
+            var data_target = day[i].getAttribute("data-target");
             var today = document.getElementById(name);
             today.setAttribute("class", "border border-danger font-weight-bold font-italic");
             today.setAttribute("title", title);
+            today.setAttribute("data-toggle", data_toggle);
+            today.setAttribute("data-target", data_target);
         };
     })();
 </script>
